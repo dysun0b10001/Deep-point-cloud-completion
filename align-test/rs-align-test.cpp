5f27d088911475a882c428cd31d7e64750c1632f
bexilin <bexilin@umich.edu>
2019-05-15 20:03:18 -0400

    align depth to color images tool

--- align-test/rs-align-test.cpp
+++ align-test/rs-align-test.cpp
@@ -14,10 +14,59 @@
 #include <librealsense2/rs.hpp>
 #include <opencv2/opencv.hpp>
 #include <array>
+#include <string>
+#include <vector>
+#ifdef WIN32
+#include <experimental/filesystem>
+namespace filesystem = std::experimental::filesystem::v1;
+#else
+#if __has_include(<filesystem>)
+#include <filesystem>
+namespace filesystem = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace filesystem = std::experimental::filesystem;
+#endif
+#endif
 
 // 3rd party header for writing png files
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+// initialization
+void initialize(int argc, char * argv[], filesystem::path & home_dir, uint32_t & frame_int)
+{
+    const std::string keys =
+        "{ home h    |       | home saving directory, required (path)                            }"
+        "{ interval i| 1     | frame interval of saved images, optional, default is 1 (uint32_t) }";
+    cv::CommandLineParser parser( argc, argv, keys );
+
+    // Check Parsing Error
+    if( !parser.check() ){
+        parser.printErrors();
+        throw std::runtime_error( "failed command arguments" );
+    }
+
+    // get home directory and create subdirectory for color and depth images
+    if( !parser.has("home")){
+        throw std::runtime_error( "no home saving directory provided");
+    }
+    else{
+        home_dir = parser.get<std::string>("home");
+        filesystem::path color_dir = home_dir.generic_string() + "/Color";
+        filesystem::path depth_dir = home_dir.generic_string() + "/Depth";
+        filesystem::create_directories(color_dir);
+        filesystem::create_directories(depth_dir);
+    }
+
+    // get frame interval (Option)
+    if( !parser.has( "interval" ) ){
+        frame_int = 1;
+    }
+    else{
+        frame_int = parser.get<uint32_t>( "interval" );
+    } 
+}
 
 
 // Draw Depth
@@ -30,7 +79,7 @@ cv::Mat drawDepth(rs2::depth_frame depth)
 }
 
 // Save Depth
-void saveDepth(cv::Mat depth_mat, rs2::depth_frame depth)
+void saveDepth(cv::Mat depth_mat, rs2::depth_frame depth, filesystem::path home_dir, uint32_t frame_num, uint32_t frame_int)
 {
     if( !depth ){
         return;
@@ -42,8 +91,9 @@ void saveDepth(cv::Mat depth_mat, rs2::depth_frame depth)
 
     // Create Save Directory and File Name
     std::ostringstream oss;
-    oss << "~/Depth_image/";
-    oss << std::setfill( '0' ) << std::setw( 6 ) << depth.get_frame_number() << ".png";
+    oss << home_dir.generic_string() << "/Depth/";
+    oss << "depth_" << frame_num/frame_int + 1 << ".png";
+    std::cout << "depth_" << frame_num/frame_int + 1 << ".png" << " saved."<<std::endl;
 
     // Scalinge
     cv::Mat scale_mat = depth_mat;
@@ -52,8 +102,7 @@ void saveDepth(cv::Mat depth_mat, rs2::depth_frame depth)
     // }
 
     // Write Depth Image
-    cv::imwrite( "rs-save-to-disk-depth.png", scale_mat );
-    std::cout << "depth written" << std::endl;
+    cv::imwrite( oss.str(), scale_mat );
 }
 /*
  This example introduces the concept of spatial stream alignment.
@@ -85,6 +134,13 @@ void render_slider(rect location, float *alpha, direction *dir);
 int main(int argc, char *argv[]) try
 {
     cv::Mat depth_mat;
+    uint32_t frame_num = 0;
+    uint32_t frame_int;
+    filesystem::path home_dir;
+    std::vector <std::string> data_list;
+
+    initialize(argc, argv, home_dir, frame_int);
+
     // Create and initialize GUI related objects
     window app(1280, 720, "RealSense Align Example"); // Simple window handling
     ImGui_ImplGlfw_Init(app, false);                  // ImGui library intializition
@@ -137,18 +193,24 @@ int main(int argc, char *argv[]) try
         // std::cout << "Saved " << png_file1.str() << std::endl;
         // std::cout << "Data depth " << depth.get_bytes_per_pixel() << std::endl;
 
-        // Draw Depth
-        depth_mat = drawDepth(depth);
-        // Save Depth
-        saveDepth(depth_mat,depth);
+        if(frame_num % frame_int == 0){
+            // Draw Depth
+            depth_mat = drawDepth(depth);
+            // Save Depth
+            saveDepth(depth_mat,depth,home_dir,frame_num,frame_int);
 
-        std::stringstream png_file2;
-        // Write images to disk
-        png_file2 << "rs-save-to-disk-color" << ".png";
-        stbi_write_png(png_file2.str().c_str(), color.get_width(), color.get_height(),
-                       color.get_bytes_per_pixel(), color.get_data(), color.get_stride_in_bytes());
-        std::cout << "Saved " << png_file2.str() << std::endl;
-        std::cout << "Data color " << color.get_bytes_per_pixel() << std::endl;
+            std::stringstream png_file2;
+            // Write color images to disk
+            png_file2 << home_dir.generic_string() << "/Color/color_" << frame_num/frame_int + 1 << ".png";
+            stbi_write_png(png_file2.str().c_str(), color.get_width(), color.get_height(),
+                        color.get_bytes_per_pixel(), color.get_data(), color.get_stride_in_bytes());
+            std::cout << "color_" << frame_num/frame_int + 1 << ".png" << " saved."<<std::endl;
+            //std::cout << "Data color " << color.get_bytes_per_pixel() << std::endl;
+
+            // add to data_list file
+            data_list.push_back(std::to_string(frame_num/frame_int + 1));
+            data_list.push_back("\n");
+        }
 
         glEnable(GL_BLEND);
         // Use the Alpha channel for blending
@@ -176,7 +238,18 @@ int main(int argc, char *argv[]) try
         ImGui_ImplGlfw_NewFrame(1);
         render_slider({15.f, app.height() - 60, app.width() - 30, app.height()}, &alpha, &dir);
         ImGui::Render();
+
+        frame_num += 1;
     }
+
+    std::ofstream file;
+    file.open(home_dir.generic_string()+"/data_list.txt");
+    for (std::vector<std::string>::iterator iter = data_list.begin(); iter < data_list.end()-1; ++iter){
+        file << *iter;
+    }
+    file.close();
+    std::cout << std::endl;
+    std::cout << "finish, "<< frame_num/frame_int + 1<< " frame(s) saved."<<std::endl;
 
     return EXIT_SUCCESS;
 }
